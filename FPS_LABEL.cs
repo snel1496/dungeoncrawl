d44fcde62600e0ab0a7279ebb95a11b845526148
snel <stsam63@gmail.com>
2025-07-18 08:08:08 -0500

    devcommit

---
+++ FPS_LABEL.cs
@@ -0,0 +1,11 @@
+using Godot;
+using System;
+
+public partial class FPS_LABEL : Label
+{
+    public override void _Process(double delta)
+    {
+
+        Text = Engine.GetFramesPerSecond().ToString();
+    }
+}
